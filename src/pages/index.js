b8c4f9ec3145fdad8d5546915a337982b92c9e01
Capabay <capabay@hotmail.com>
2021-04-12 01:39:38 +0100

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import landing from  '../images/landing_bg.jpg'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import { makeStyles, createMuiTheme, ThemeProvider, MuiThemeProvider, responsiveFontSizes } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid';
 import { Typography, Button, withTheme } from "@material-ui/core"
 import Paper from '@material-ui/core/Paper'
@@ -21,37 +21,21 @@ const useStyles = makeStyles({
     marginTop: 400,
     color: 'white',
     padding: '10px 60px',
-    background: 'linear-gradient(45deg, #042d73, #04befe)'
+    background: 'linear-gradient(45deg, #042d73, #04befe)',
+    "@media (max-width: 600px)": {
+      marginTop: 185,
+      marginLeft: 85,
+    }
   }
 }) 
-
-const useStyles2 = makeStyles({
-  doe:{
-    border: 0,
-    borderRadius: 22,
-    marginLeft: 140,
-    color: 'white',
-    padding: '10px 80px',
-    background: 'linear-gradient(45deg, #4481eb, #04befe)'
-  }
-}) 
-
-function DoeStyled(){
-  const John = useStyles2()
-  return <Button className={John.doe}>My button test</Button>
-}
 
 function BtnStyled(){
   const classes = useStyles()
   return <Button className={classes.root}>My button test</Button>
 }
-const theme = createMuiTheme({
-  Typography: {
-    
-  },
 
-})
-
+// const theme = createMuiTheme();
+// theme = responsiveFontSizes(theme);
 const IndexPage = () => {
 
   const [loading, setLoading] = useState(false)
@@ -65,7 +49,7 @@ const IndexPage = () => {
       {
 
        return (
-         
+        
         <Layout> 
         <div className="sticky-nav">
         
@@ -73,7 +57,7 @@ const IndexPage = () => {
           <SEO title="Home" />
           <div>
             <Grid container className="landing">
-              <Grid item sx={12} className="try">
+              <Grid item className="try">
                   <img src={landing} />       
                   <Typography
                   variant= "h1"
@@ -84,7 +68,9 @@ const IndexPage = () => {
             </Grid>
           </div>   
           <Home2 />
-        </Layout>)
+        </Layout>
+    
+        )
       }
   }
 export default IndexPage
