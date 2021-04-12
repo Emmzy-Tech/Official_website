cf7b46b0f97b671af079d5e92821f08ef2538c67
Capabay <capabay@hotmail.com>
2021-04-12 02:44:55 +0100

    in progress

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -20,30 +20,7 @@ const Header = ({ siteTitle }) => {
 
  return(
   <header>
-    <Navbar expand="lg" className="navbar active">
-    <Navbar.Brand href="#home"><img src={logo} className="logo"/></Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbarResponsive" />
-    <Navbar.Collapse id="navbarResponsive">
-      <Nav as="ul" className="ml-auto">
-      <Nav.Item as="li">
-          <Link to="/" className="nav-link" activeClassName="active">Home</Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-      <Link to="/page-2" className="nav-link" activeClassName="active">About Us</Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-      <Link to="#" className="nav-link" activeClassName="active"></Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-      <Link to="#" className="nav-link" activeClassName="active"></Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-      <Link to="#" className="nav-link" activeClassName="active"></Link>
-      </Nav.Item>
-      </Nav>
-    </Navbar.Collapse>
-    </Navbar>
-    
+      
   </header>
 )
  }
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,12 +18,12 @@ const useStyles = makeStyles({
     border: 0,
     borderRadius: 22,
     marginLeft: 140,
-    marginTop: 400,
+    marginTop: 520,
     color: 'white',
     padding: '10px 60px',
     background: 'linear-gradient(45deg, #042d73, #04befe)',
     "@media (max-width: 600px)": {
-      marginTop: 185,
+      marginTop: 225,
       marginLeft: 85,
     }
   }
