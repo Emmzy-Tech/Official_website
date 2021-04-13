44afb59081d3610932ce17a7a7fed0e7ff606afe
Capabay <capabay@hotmail.com>
2021-04-13 17:02:43 +0100

    darkMode101

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,10 +8,11 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./navbar/index"
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { useState, useEffect } from 'react'
+import styled, {ThemeProvider} from 'styled-components'
+import {lightTheme, darkTheme, GlobalStyles} from './theme.js'
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -22,10 +23,18 @@ const Layout = ({ children }) => {
       }
     }
   `)
+  // const styledApp = styled.div`
+  
+  // `
+  const [theme, setTheme] = useState('dark')
+
+  const themeToggler = ()=>{
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+          <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
@@ -42,6 +51,7 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
       </div>
+     
     </>
   )
 }
---
+++ src/components/theme.js
@@ -0,0 +1,16 @@
+import { createStyles } from '@material-ui/styles'
+import {createGlobalStyles} from 'styled-components'
+import { GlobalStyles } from './globalStyles'
+
+export const lightTheme = {
+    body: '#FFF',
+    text: '#363537',
+    toggleBorder: '#FFF',
+    background: '#363537',
+}
+export const darkTheme = {
+    body: '#363537',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    background: '#999',
+}
