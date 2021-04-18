f622a6b856ad227047754a595d40b8460ab7a76f
Capabay <capabay@hotmail.com>
2021-04-18 21:40:15 +0100

    working without themeProvider

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -45,9 +45,7 @@ const Header = ({ siteTitle }) => {
   }
     return(
       <>
-      <ThemeProvider theme={theme === "light" ? darkTheme : lightTheme}>
-      <GlobalStyles/>
-      <header>
+            <header>
           <AppBar position="sticky">
             <Toolbar>
             <Typography variant="h6" style={{flexGrow: 1}}>Capacity</Typography>
@@ -65,7 +63,7 @@ const Header = ({ siteTitle }) => {
           </AppBar>
          
       </header>
-      </ThemeProvider>
+    
       </>
     )
 }
