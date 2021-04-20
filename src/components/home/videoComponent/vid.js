a88b4582b75bff1e5e983723057b398a7039bfce
Capabay <capabay@hotmail.com>
2021-04-20 06:37:32 +0100

    pages

---
+++ src/components/home/videoComponent/vid.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme)=>({
+    paperplay:{
+     
+    },
+}))
+
+const Vid = (props)=>{
+    const classes = useStyles()
+    return (
+        <>
+           <Layout>
+           <div className="page-wrap">
+           <Grid container spacing={2}>
+           <Grid item md={6} xs={12}>
+                <Paper className={classes.paperplay}>
+            
+                </Paper>
+            </Grid>
+
+            <Grid item md={6} xs={12}>
+            <Paper className={classes.paperplay}>        
+            </Paper>
+            </Grid>
+           </Grid> 
+           </div>          
+           </Layout> 
+        </>
+    )
+}
+
+Vid.propTypes = {
+
+}
+
+export default Vid
+
