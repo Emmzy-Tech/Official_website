3a7b147523b1fbfbb370a8f07b1262741c94a84c
Capabay <capabay@hotmail.com>
2021-04-24 01:17:15 +0100

    +main

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Layout from "../components/layout"
+import "../components/main.css"
 import SEO from "../components/seo"
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
